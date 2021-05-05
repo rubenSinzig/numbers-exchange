0bae15c35552ff95ac99c6860f478c8524c71f11
rubenSinzig <ruben.sinzig@posteo.de>
2021-05-05 13:20:52 +0200

    js -> function

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const year = new Date().getFullYear();
 document.querySelector(".year").innerHTML = year;
 
 //
+const userInput = document.querySelector("#userInput");
+const convert = document.querySelector("#convert");
+let result = document.querySelector(".result");
+//
+const exChange = (e) => {
+  e.preventDefault();
+  console.log(userInput.value);
+  if (userInput.value != "") {
+    result.innerHTML += `Your number is ${userInput.value}<br>`;
+  } else {
+    userInput.placeholder = "Please FIRST insert a binary number";
+  }
+};
+//
+convert.addEventListener("click", exChange);
